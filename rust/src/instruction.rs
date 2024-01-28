534f58cc7cdaa069e36435766765384c14fc7675
angie <angheloalf95@gmail.com>
2024-01-28 12:29:44 -0300

    clippy

--- rust/src/instruction.rs
+++ rust/src/instruction.rs
@@ -458,14 +458,14 @@ impl Instruction {
         utils::shiftr(self.flags, 0, 2).try_into().unwrap()
     }
     pub fn flags_set_r5900_disasm_as_data(&mut self, value: utils::TrinaryValue) {
-        self.flags = utils::bitrepack(self.flags, value.try_into().unwrap(), 0, 2);
+        self.flags = utils::bitrepack(self.flags, value.into(), 0, 2);
     }
 
     pub fn flags_get_r5900_use_dollar(&self) -> utils::TrinaryValue {
         utils::shiftr(self.flags, 2, 2).try_into().unwrap()
     }
     pub fn flags_set_r5900_use_dollar(&mut self, value: utils::TrinaryValue) {
-        self.flags = utils::bitrepack(self.flags, value.try_into().unwrap(), 2, 2);
+        self.flags = utils::bitrepack(self.flags, value.into(), 2, 2);
     }
 
     pub fn instr_id_type_name(&self) -> &'static str {
