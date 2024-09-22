7c372adc7210291d9bba0db9cac5a080fe4d81ef
angie <angheloalf95@gmail.com>
2024-09-22 14:50:39 -0300

    reorder some register stuff

--- src/rabbitizer/generated/enum_register_cop0.rs
+++ src/rabbitizer/generated/enum_register_cop0.rs
@@ -3,6 +3,7 @@
 
 /* Automatically generated. DO NOT MODIFY */
 
+use crate::RegisterDescriptor;
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
 #[allow(non_camel_case_types)]
 #[allow(clippy::exhaustive_enums)]
@@ -40,3 +41,147 @@ pub enum RegisterCop0 {
     ErrorEPC,
     Reserved31,
 }
+pub static COP0_REGISTERS: [RegisterDescriptor; 32] = {
+    let mut table = [RegisterDescriptor::default(); 32];
+    table[RegisterCop0::Index as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("Index", 0, concat!("$", "0"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Random as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("Random", 1, concat!("$", "1"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::EntryLo0 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("EntryLo0", 2, concat!("$", "2"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::EntryLo1 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("EntryLo1", 3, concat!("$", "3"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Context as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("Context", 4, concat!("$", "4"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::PageMask as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("PageMask", 5, concat!("$", "5"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Wired as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("Wired", 6, concat!("$", "6"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Reserved07 as usize] = RegisterDescriptor {
+        is_reserved: true,
+        ..RegisterDescriptor::new("Reserved07", 7, concat!("$", "7"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::BadVaddr as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("BadVaddr", 8, concat!("$", "8"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Count as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("Count", 9, concat!("$", "9"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::EntryHi as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("EntryHi", 10, concat!("$", "10"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Compare as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("Compare", 11, concat!("$", "11"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Status as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("Status", 12, concat!("$", "12"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Cause as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("Cause", 13, concat!("$", "13"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::EPC as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("EPC", 14, concat!("$", "14"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::PRevID as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("PRevID", 15, concat!("$", "15"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Config as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("Config", 16, concat!("$", "16"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::LLAddr as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("LLAddr", 17, concat!("$", "17"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::WatchLo as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("WatchLo", 18, concat!("$", "18"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::WatchHi as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("WatchHi", 19, concat!("$", "19"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::XContext as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("XContext", 20, concat!("$", "20"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Reserved21 as usize] = RegisterDescriptor {
+        is_reserved: true,
+        ..RegisterDescriptor::new("Reserved21", 21, concat!("$", "21"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Reserved22 as usize] = RegisterDescriptor {
+        is_reserved: true,
+        ..RegisterDescriptor::new("Reserved22", 22, concat!("$", "22"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Reserved23 as usize] = RegisterDescriptor {
+        is_reserved: true,
+        ..RegisterDescriptor::new("Reserved23", 23, concat!("$", "23"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Reserved24 as usize] = RegisterDescriptor {
+        is_reserved: true,
+        ..RegisterDescriptor::new("Reserved24", 24, concat!("$", "24"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Reserved25 as usize] = RegisterDescriptor {
+        is_reserved: true,
+        ..RegisterDescriptor::new("Reserved25", 25, concat!("$", "25"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::PErr as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("PErr", 26, concat!("$", "26"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::CacheErr as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("CacheErr", 27, concat!("$", "27"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::TagLo as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("TagLo", 28, concat!("$", "28"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::TagHi as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("TagHi", 29, concat!("$", "29"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::ErrorEPC as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new("ErrorEPC", 30, concat!("$", "30"))
+    }
+    .check_panic_chain();
+    table[RegisterCop0::Reserved31 as usize] = RegisterDescriptor {
+        is_reserved: true,
+        ..RegisterDescriptor::new("Reserved31", 31, concat!("$", "31"))
+    }
+    .check_panic_chain();
+    let mut i = 0;
+    while i < 32 {
+        assert!(table[i].value as usize == i, "Broken register index?");
+        i += 1;
+    }
+    table
+};
--- src/rabbitizer/generated/enum_register_r5900allegrex_vf.rs
+++ src/rabbitizer/generated/enum_register_r5900allegrex_vf.rs
@@ -3,6 +3,7 @@
 
 /* Automatically generated. DO NOT MODIFY */
 
+use crate::RegisterDescriptor;
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
 #[allow(non_camel_case_types)]
 #[allow(clippy::exhaustive_enums)]
@@ -40,3 +41,140 @@ pub enum RegisterR5900VF {
     vf30 = 30,
     vf31 = 31,
 }
+pub static R5900_VF_REGISTERS: [RegisterDescriptor; 32] = {
+    let mut table = [RegisterDescriptor::default(); 32];
+    table[RegisterR5900VF::vf0 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf0"), 0, concat!("$vf", "0"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf1 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf1"), 1, concat!("$vf", "1"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf2 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf2"), 2, concat!("$vf", "2"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf3 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf3"), 3, concat!("$vf", "3"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf4 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf4"), 4, concat!("$vf", "4"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf5 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf5"), 5, concat!("$vf", "5"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf6 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf6"), 6, concat!("$vf", "6"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf7 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf7"), 7, concat!("$vf", "7"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf8 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf8"), 8, concat!("$vf", "8"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf9 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf9"), 9, concat!("$vf", "9"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf10 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf10"), 10, concat!("$vf", "10"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf11 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf11"), 11, concat!("$vf", "11"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf12 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf12"), 12, concat!("$vf", "12"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf13 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf13"), 13, concat!("$vf", "13"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf14 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf14"), 14, concat!("$vf", "14"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf15 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf15"), 15, concat!("$vf", "15"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf16 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf16"), 16, concat!("$vf", "16"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf17 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf17"), 17, concat!("$vf", "17"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf18 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf18"), 18, concat!("$vf", "18"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf19 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf19"), 19, concat!("$vf", "19"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf20 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf20"), 20, concat!("$vf", "20"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf21 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf21"), 21, concat!("$vf", "21"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf22 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf22"), 22, concat!("$vf", "22"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf23 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf23"), 23, concat!("$vf", "23"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf24 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf24"), 24, concat!("$vf", "24"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf25 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf25"), 25, concat!("$vf", "25"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf26 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf26"), 26, concat!("$vf", "26"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf27 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf27"), 27, concat!("$vf", "27"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf28 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf28"), 28, concat!("$vf", "28"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf29 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf29"), 29, concat!("$vf", "29"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf30 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf30"), 30, concat!("$vf", "30"))
+    }
+    .check_panic_chain();
+    table[RegisterR5900VF::vf31 as usize] = RegisterDescriptor {
+        ..RegisterDescriptor::new(concat!("$", "vf31"), 31, concat!("$vf", "31"))
+    }
+    .check_panic_chain();
+    let mut i = 0;
+    while i < 32 {
+        assert!(table[i].value as usize == i, "Broken register index?");
+        i += 1;
+    }
+    table
+};
